b3b39b053a5ba11dff306c817186e5b0d233e0e7
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-12-28 10:46:43 +0200

    Function to trigger on file upload to storage bucket

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,14 +6,33 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
+import {initializeApp} from "firebase-admin/app";
+import {setGlobalOptions} from "firebase-functions/v2";
 
-import {onRequest} from "firebase-functions/v2/https";
+import {onObjectFinalized} from "firebase-functions/v2/storage";
 import * as logger from "firebase-functions/logger";
+import {getStorage} from "firebase-admin/storage";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+setGlobalOptions({region: "europe-central2"});
+initializeApp();
+
+
+export const onFileUpload = onObjectFinalized((event) => {
+  logger.info(event);
+  logger.info("Got file uploaded to bucket", event.bucket);
+  logger.info("Object data is ", event.data);
+  const bucketName = event.data.bucket;
+  const filePath = event.data.name;
+  const readStream = getStorage()
+    .bucket(bucketName)
+    .file(filePath)
+    .createReadStream();
+  readStream.pipe(process.stdout);
 });
+
+// I want to create a function which trigger on firebase storage bucket upload
+// https://firebase.google.com/docs/storage/web/upload-files
+

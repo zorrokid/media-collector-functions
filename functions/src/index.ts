fb4bef42b9c359fd8bbe34b30355abf7b746b1cd
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-12-28 15:31:06 +0200

    write history entry and delete source file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,14 +10,23 @@ import {initializeApp} from "firebase-admin/app";
 import {setGlobalOptions} from "firebase-functions/v2";
 
 import {onObjectFinalized} from "firebase-functions/v2/storage";
-import * as logger from "firebase-functions/logger";
 import {getStorage} from "firebase-admin/storage";
+import {getFirestore} from "firebase-admin/firestore";
+
+import * as logger from "firebase-functions/logger";
+import * as path from "path";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
 setGlobalOptions({region: "europe-central2"});
 initializeApp();
+
+type ImportHistoryEntry = {
+  time: Date;
+  fileName: string;
+  itemsImported: number;
+}
 
 
 export const onFileUpload = onObjectFinalized((event) => {
@@ -26,11 +35,29 @@ export const onFileUpload = onObjectFinalized((event) => {
   logger.info("Object data is ", event.data);
   const bucketName = event.data.bucket;
   const filePath = event.data.name;
-  const readStream = getStorage()
+  const file = getStorage()
     .bucket(bucketName)
-    .file(filePath)
+    .file(filePath);
+  const readStream = file
     .createReadStream();
+
   readStream.pipe(process.stdout);
+
+  // TODO validate and parse file content
+  // TODO write data to firestore
+
+  const document = getFirestore().collection("importHistory").doc();
+  const importHistoryEntry: ImportHistoryEntry = {
+    time: new Date(),
+    fileName: path.basename(filePath),
+    itemsImported: 0,
+  };
+  document.set(importHistoryEntry);
+  file.delete().then(() => {
+    logger.log("Upload file deleted successfully");
+  }).catch((error) => {
+    logger.error("Error deleting upload file", error);
+  });
 });
 
 // I want to create a function which trigger on firebase storage bucket upload
